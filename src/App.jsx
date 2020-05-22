ba7944908db3395366c7810f680537b8f6b83252
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-05-22 17:33:44 +0300

    add styles and randomizer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.scss';
+import { Randomizer } from './components/Randomizer';
 
 const App = () => (
-  <>
-    <div>MTI</div>
-  </>
+  <div className="app-wrapper">
+    <div className="app-wrapper__content"><Randomizer /></div>
+    <div className="app-wrapper__favorite">Favorite</div>
+  </div>
 );
 
 export default App;
---
+++ src/components/Randomizer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.scss';
+
+export const Randomizer = () => {
+
+  return (
+    <div className="randomizer">
+      <h1 className="randomizer__heading">MSI 2020</h1>
+      <p className="randomizer__title">Hey!</p>
+      <p className="randomizer__underTitle">
+        Let’s try to find a joke for you:
+      </p>
+    </div>
+  );
+};
