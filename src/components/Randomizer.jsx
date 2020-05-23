a9f74bee799f5b2809a051b1f25df9e40fd45b67
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-05-23 21:55:12 +0300

    fetch random joke from API

--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import '../App.scss';
+import { getJoke } from '../api';
 
-export const Randomizer = () => {
-
-  return (
-    <div className="randomizer">
-      <h1 className="randomizer__heading">MSI 2020</h1>
-      <p className="randomizer__title">Hey!</p>
-      <p className="randomizer__underTitle">
+export const Randomizer = () => (
+  <div className="randomizer">
+    <h1 className="randomizer__heading">MSI 2020</h1>
+    <p className="randomizer__title">Hey!</p>
+    <p className="randomizer__underTitle">
         Let’s try to find a joke for you:
-      </p>
-    </div>
-  );
-};
+    </p>
+    <button
+      type="button"
+      className="randomizer__button"
+      onClick={getJoke}
+    >
+Get a joke
+    </button>
+  </div>
+);
